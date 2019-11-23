baecfe6cca701e033c3015ac825136f40c3c2fee
Liangwei Chen <lchenbb@connect.ust.hk>
2019-11-24 04:34:33 +0800

    Finish auction

---
+++ project/auction/src/TestAgent/MainAuction.java
@@ -0,0 +1,16 @@
+package TestAgent;
+
+public class MainAuction {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		if (args.length > 0) {
+			logist.LogistPlatform.main(args);
+		} else {
+			String[] defaultArgs = { "config/auction.xml", "auction-our", "auction-our"};
+			logist.LogistPlatform.main(defaultArgs);
+		}
+	}
+}
